4b283ea120929dbe8195bb0a33b716cf97ad4589
Brian Kim <brianjunewookim@gmail.com>
2021-02-26 16:44:38 -0800

    Update logic for args in random number generation

--- src/commandLogic.js
+++ src/commandLogic.js
@@ -1,5 +1,5 @@
 const randomNumberGenerator = (num) => {
-  if (!num || typeof num !== 'number') {
+  if (!num || isNaN(num)) {
     return Math.floor(Math.random() * 101);
   } else {
     return Math.floor(Math.random() * (num + 1));
