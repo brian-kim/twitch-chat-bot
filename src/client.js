1a1b6f654888c541f8c64e51bc7ec67a1d7410f6
Brian Kim <brianjunewookim@gmail.com>
2021-02-28 22:04:41 -0800

    Create client.js for client initialization

---
+++ src/client.js
@@ -0,0 +1,10 @@
+const tmi = require('tmi.js');
+
+const { clientVars } = require('./variables');
+
+//Twitch docs can be found here: https://dev.twitch.tv/docs/
+const client = new tmi.Client(clientVars);
+
+module.exports = {
+  client
+}
