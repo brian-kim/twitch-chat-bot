6160cc4c5ba2d3919f5a75e496e91092526e45fc
Brian Kim <brianjunewookim@gmail.com>
2021-03-02 15:53:02 -0800

    Add !slots, !title, and !game commands

--- src/twitchChatBot.js
+++ src/twitchChatBot.js
@@ -31,25 +31,34 @@ client.on('message', (channel, user, msg, self) => {
     const generatePercentage = helpers.randomNumberGenerator();
     client.say(channel, `There is ${generatePercentage}% love between ${user.username} and ${commandArgs}.`);
   // Get the current stream title
-  } else if (command === '!title') {
-    axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${process.env.STREAMER_CHANNEL_ID}`, streamHeaders)
-      .then(res => client.say(channel, `Current title: ${res.data.data[0].title}`))
-      .catch(err => console.log(err));
-    // Get current stream game
-  } else if (command === '!game') {
-    axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${process.env.STREAMER_CHANNEL_ID}`, streamHeaders)
-      .then(res => client.say(channel, `Current game: ${res.data.data[0].game_name}`))
+  } else if (command === '!slots') {
+    axios.get(`https://decapi.me/bttv/emotes/${process.env.STREAMER_USERNAME}`)
+      .then(res => {
+        const emotes = res.data.split(' ');
+        const slotsResult = helpers.slotMachine(emotes);
+        client.say(channel, `/me ${slotsResult.slots[0]} | ${slotsResult.slots[1]} | ${slotsResult.slots[2]}`);
+        client.say(channel, slotsResult.result);
+      })
       .catch(err => console.log(err))
-  // Allow broadcaster or mod to change stream title
-  } else if ((user.username === process.env.STREAMER_USERNAME || user.mod) && command === '!title') {
-    const newInfo = {
-      'title': commandArgs
-    };
-    helpers.changeStreamInfo(newInfo);
-    client.say(channel, `Title has been changed to "${commandArgs}"`);
-  // Allow broadcaster or mod to change stream game
-  } else if ((user.username === process.env.STREAMER_USERNAME || user.mod) && command === '!game') {
-    helpers.changeStreamGame(commandArgs);
-    client.say(channel, `Game has been changed to "${commandArgs}"`);
-  }
+      // Allow broadcaster or mod to change stream title
+    } else if ((user.username === process.env.STREAMER_USERNAME || user.mod) && (command === '!title' && commandArgs !== '')) {
+      const newInfo = {
+        'title': commandArgs
+      };
+      helpers.changeStreamInfo(newInfo);
+      client.say(channel, `Title has been changed to "${commandArgs}"`);      
+      // Allow broadcaster or mod to change stream game
+    } else if ((user.username === process.env.STREAMER_USERNAME || user.mod) && (command === '!game' && commandArgs !== '')) {
+      helpers.changeStreamGame(commandArgs);
+      client.say(channel, `Game has been changed to "${commandArgs}"`);
+    } else if (command === '!title') {
+      axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${process.env.STREAMER_CHANNEL_ID}`, streamHeaders)
+        .then(res => client.say(channel, `Current title: ${res.data.data[0].title}`))
+        .catch(err => console.log(err))
+      // Get current stream game
+    } else if (command === '!game') {
+      axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${process.env.STREAMER_CHANNEL_ID}`, streamHeaders)
+        .then(res => client.say(channel, `Current game: ${res.data.data[0].game_name}`))
+        .catch(err => console.log(err))
+    }
 });
