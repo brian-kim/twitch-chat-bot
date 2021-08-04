311cf711aaf9867cc1ca6abe3e28d0eb9ce54db6
brian-kim <brianjunewookim@gmail.com>
2021-08-04 12:39:18 -0700

    Add space in comment for consistency

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ const tmi = require('tmi.js');
 
 const { clientVars } = require('./variables');
 
-//Twitch docs can be found here: https://dev.twitch.tv/docs/
+// Twitch docs can be found here: https://dev.twitch.tv/docs/
 const client = new tmi.Client(clientVars);
 
 module.exports = {
